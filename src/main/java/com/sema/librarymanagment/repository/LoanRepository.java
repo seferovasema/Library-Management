acf616a74c9cf9e5075dd8b7184541c1c0e0f1c9
seferovasema <sema.seferova05@mail.ru>
2026-08-01 00:51:16 +0400

    feat: add overdue loan query

--- src/main/java/com/sema/librarymanagment/repository/LoanRepository.java
+++ src/main/java/com/sema/librarymanagment/repository/LoanRepository.java
@@ -4,9 +4,12 @@ import com.sema.librarymanagment.entity.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
     List<Loan> findByReturnedFalse();
+
+    List<Loan> findByReturnedFalseAndDueDateBefore(LocalDate date);
 }
