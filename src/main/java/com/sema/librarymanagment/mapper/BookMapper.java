a8e8fceb0aabfa5eff2487d22f6710f5122383ce
seferovasema <sema.seferova05@mail.ru>
2026-08-01 18:24:23 +0400

    feat: map book categories in mapper

--- src/main/java/com/sema/librarymanagment/mapper/BookMapper.java
+++ src/main/java/com/sema/librarymanagment/mapper/BookMapper.java
@@ -3,13 +3,22 @@ package com.sema.librarymanagment.mapper;
 import com.sema.librarymanagment.dto.request.BookRequestDto;
 import com.sema.librarymanagment.dto.response.BookResponseDto;
 import com.sema.librarymanagment.entity.Book;
+import com.sema.librarymanagment.entity.Category;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface BookMapper {
     @Mapping(source = "author.name", target = "authorName")
+    @Mapping(source = "categories", target = "categories")
     BookResponseDto toDto(Book book);
 
     Book toEntity(BookRequestDto dto);
-}
+
+    default List<String> map(List<Category> categories) {
+        return categories.stream()
+                .map(Category::getName)
+                .toList();
+}}
