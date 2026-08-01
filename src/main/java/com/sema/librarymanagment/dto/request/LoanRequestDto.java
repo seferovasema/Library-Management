22495f2b5ef502f773a142c1f76518a3aba1a202
seferovasema <sema.seferova05@mail.ru>
2026-08-01 22:40:45 +0400

    feat: complete loan request dto

--- src/main/java/com/sema/librarymanagment/dto/request/LoanRequestDto.java
+++ src/main/java/com/sema/librarymanagment/dto/request/LoanRequestDto.java
@@ -1,4 +1,25 @@
 package com.sema.librarymanagment.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class LoanRequestDto {
+    @NotNull
+    Long bookId;
+
+    @NotNull
+    Long memberId;
+
+    @NotNull
+    LocalDate dueDate;
 }
