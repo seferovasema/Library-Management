2a654da18d7e20f457681fb1494dd8e851e17d1e
seferovasema <sema.seferova05@mail.ru>
2026-08-01 18:29:18 +0400

    feat: enable specifications for books

--- src/main/java/com/sema/librarymanagment/repository/BookRepository.java
+++ src/main/java/com/sema/librarymanagment/repository/BookRepository.java
@@ -2,12 +2,13 @@ package com.sema.librarymanagment.repository;
 
 import com.sema.librarymanagment.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
     List<Book> findByAuthorId(Long authorId);
 
 }
