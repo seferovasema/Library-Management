a6dbf0d922b42e7693927855233430b30cc38d3d
seferovasema <sema.seferova05@mail.ru>
2026-08-01 17:49:51 +0400

    feat: add loan service interface

--- src/main/java/com/sema/librarymanagment/service/LoanService.java
+++ src/main/java/com/sema/librarymanagment/service/LoanService.java
@@ -1,4 +1,14 @@
 package com.sema.librarymanagment.service;
 
+import com.sema.librarymanagment.dto.response.LoanResponseDto;
+
+import java.util.List;
+
 public interface LoanService {
+
+    List<LoanResponseDto> getActiveLoans();
+
+    List<LoanResponseDto> getOverdueLoans();
+
+    List<LoanResponseDto> getActiveLoansByMember(Long memberId);
 }
